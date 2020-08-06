74acad4f9101d3efa2e3e42cc722beac6ece8bda
hemadamostafa <courses.0n.line.by.tata@gmail.com>
2020-08-05 17:38:43 -0700

    adding fields

--- job/models.py
+++ job/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+JOB_TYPE = (
+    ("full time","full time"),
+    ("part time","part time"),
+)
+class job(models.Model):
+    title = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=15,choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    publish_at = models.DateTimeField(auto_now=True)
+    salary = models.IntegerField(default=0)
+    exper = models.IntegerField(default=1)
+    Vacancy = models.IntegerField(default=1)
+
+
+    def __str__(self):
+        return self.title
+    
